5aeb967ac6c55c9d47d3a74fe08c2cbeb94e349c
Jason Horn <jhorn@redhat.com>
2020-06-19 11:26:40 -0400

    file layout

--- bases/bootstrap/boot/script.sh
+++ bases/bootstrap/boot/script.sh
@@ -4,19 +4,11 @@
 # oc create sa bootstrap -n default
 #  oc adm policy add-cluster-role-to-user cluster-admin serviceaccount:default:bootstrap
 # Infrastructure Nodes
-infraNodes="compute-0 compute-1 compute-2"
+infraNodes="{$2}"
 
 # git Repository
 gitRepo="https://github.com/hornjason/argocd-lab.git"
 project="argocd-lab"
-
-# Update Identity Secret
-clientSecret="${1}"
-# convert to base64 for kubesealer
-identitySecret=$(echo ${clientSecret} |base64)
-# Create kube sealed secrets for sensitive items
-cd /${project}/bases/bootstrap/identity
-
 
 # git clone repo 
 git clone ${gitRepo}
@@ -26,11 +18,29 @@ cd ${project}
 # Kubesealer to create sealed secret 
 # Push back to git repo
 cd overlays/default/kubesealer
-kustomize build ./ | oc apply -f -
+kustomize build --reorder none ./ | oc apply -f -# Update Identity Secret
+
+
+clientSecret="${1}"
+# convert to base64 for kubesealer
+identitySecret=$(echo ${clientSecret} |base64)
+# Create kube sealed secrets for sensitive items 
+# touch done /run/done and invoke from outside of pod
+# pull file over and continue parent script 
+cd /${project}/bases/bootstrap/identitySecret
 
 # Install ArgoCD
+cd /${project}/overlays/default/argocd 
+kustomize build ./ --reorder none | oc apply -f -
+#
+ while [[ "$(oc get deploy argocd-operator -o template --template='{{ .status.availableReplicas }}' -n argocd)" < 1 ]]
+ do 
+    echo "Waiting for operator to become available"
+ done;
+
+# Install argo applications to start syncing
 cd /${project}/overlays/argocd 
-kustomize build ./ | oc apply -f -
+kustomize build ./ --reorder none | oc apply -f -
 
 #Label infra nodes & remove worker
 oc label nodes ${infraNodes} node-role.kubernetes.io/infra= node-role.kubernetes.io/worker- 
