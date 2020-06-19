d765c9c284cbadec85e304d93f1503bbaf8dd26c
Jason Horn <jhorn@redhat.com>
2020-06-19 15:56:48 -0400

    updated scripts

--- bases/bootstrap/boot/script.sh
+++ bases/bootstrap/boot/script.sh
@@ -3,8 +3,18 @@
 # 1. create sa for bootstrap depolyment.yaml 
 # oc create sa bootstrap -n default
 #  oc adm policy add-cluster-role-to-user cluster-admin serviceaccount:default:bootstrap
+# Identity Provider Secret
+clientSecret="${1}"
+
 # Infrastructure Nodes
 infraNodes="{$2}"
+
+
+# Full Chaing TLS Cert 
+# tlsCertFile=
+
+# TLS Key
+# tlsKeyFile=
 
 # git Repository
 gitRepo="https://github.com/hornjason/argocd-lab.git"
@@ -19,17 +29,21 @@ cd ${project}
 # Push back to git repo
 cd overlays/default/kubesealer
 kustomize build --reorder none ./ | oc apply -f -# Update Identity Secret
+ while [[ "$(oc get deploy sealed-secrets-controller -o template --template='{{ .status.availableReplicas }}' -n argocd)" < 1 ]]
+ do 
+    echo "Waiting for operator to become available"
+ done;
 
-
-clientSecret="${1}"
-# convert to base64 for kubesealer
-identitySecret=$(echo ${clientSecret} |base64)
 # Create kube sealed secrets for sensitive items 
 # touch done /run/done and invoke from outside of pod
 # pull file over and continue parent script 
 cd /${project}/bases/bootstrap/identitySecret
 echo "${clientSecret}" | oc  create secret generic idp-secret \
 --from-file=clientSecret=/dev/stdin -o yaml | kubeseal - -o yaml>/run/idp_sealed_secret.yaml
+
+# Create sealed named certs 
+# tls.crt & tls.key 
+# pass in both file locations tlsKeyFile= tls
 
 # Install ArgoCD
 cd /${project}/overlays/default/argocd 
